78bc56ef7742f7ae5b410be391353ec685717953
conert mavuso <conertmavuso@gmail.com>
2025-01-03 20:05:16 +0200

    got javascript into a diff file

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-// console.log(018 - 015 - 0025)
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const navbar = document.querySelector('.navbar');
+    const overlay = document.querySelector('.overlay');
+    const links = document.querySelectorAll('.navbar a');
+  
+    if (hamburgerMenu) {
+      hamburgerMenu.addEventListener('click', () => {
+        navbar.classList.toggle('active');
+        if (overlay) overlay.classList.toggle('active');
+      });
+    }
+  
+    links.forEach((link) => {
+      link.addEventListener('click', () => {
+        navbar.classList.remove('active');
+        if (overlay) overlay.classList.remove('active');
+      });
+    });
+  });
+  
